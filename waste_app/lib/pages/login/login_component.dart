96e5718bbfce08006393be5f8559d2d467b144a2
Pitol <lucascpitol0@gmail.com>
2020-04-16 20:24:30 -0300

    DP-DEV-2 tarefa 1: progress

--- waste_app/lib/pages/login/login_component.dart
+++ waste_app/lib/pages/login/login_component.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginComponent extends StatefulWidget {
-
   @override
   _LoginComponentState createState() => _LoginComponentState();
 }
@@ -9,7 +8,35 @@ class LoginComponent extends StatefulWidget {
 class _LoginComponentState extends State<LoginComponent> {
   @override
   Widget build(BuildContext context) {
-    return Text('login');
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  height: 230.0,
+                  margin: EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 100),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade50,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0, 0),
+                        blurRadius: 2,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
