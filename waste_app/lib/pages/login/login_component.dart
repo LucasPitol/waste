754abe9f6ff1081d5642f78f3ecbe706f3fc6774
Pitol <lucascpitol0@gmail.com>
2020-04-16 20:39:18 -0300

    DP-DEV-2 tarefa 1: progress

--- waste_app/lib/pages/login/login_component.dart
+++ waste_app/lib/pages/login/login_component.dart
@@ -6,9 +6,19 @@ class LoginComponent extends StatefulWidget {
 }
 
 class _LoginComponentState extends State<LoginComponent> {
+
+  double defaultMargin = 10.0;
+  BorderRadius defaultBorderRadius = BorderRadius.all(Radius.circular(50.0));
+  Color mainColor = Colors.grey.shade50;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        cursorColor: Colors.deepPurple,
+        primaryColor: Colors.deepPurple,
+        textSelectionHandleColor: Colors.deepPurple,
+      ),
       home: Scaffold(
         body: Stack(
           children: <Widget>[
@@ -21,13 +31,37 @@ class _LoginComponentState extends State<LoginComponent> {
                   height: 230.0,
                   margin: EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 100),
                   decoration: BoxDecoration(
-                    color: Colors.grey.shade50,
+                    color: mainColor,
                     borderRadius: BorderRadius.all(Radius.circular(20)),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey,
+                        color: Colors.grey.shade400,
                         offset: Offset(0, 0),
                         blurRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: defaultMargin,
+                            bottom: defaultMargin,
+                            left: 20,
+                            right: 20),
+                        child: TextField(
+                          // controller: _loginForm.userMail,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: defaultBorderRadius,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: defaultBorderRadius,
+                              borderSide: BorderSide(color: Colors.grey[100]),
+                            ),
+                            labelText: 'e-mail',
+                          ),
+                        ),
                       ),
                     ],
                   ),
