7ab04c594a770857bb24f4b5dd9009ff63c0bff7
Pitol <lucascpitol0@gmail.com>
2020-04-16 20:00:55 -0300

    DP-DEV-2 tarefa 1: progress

---
+++ waste_app/lib/pages/login/login_component.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginComponent extends StatefulWidget {
+
+  @override
+  _LoginComponentState createState() => _LoginComponentState();
+}
+
+class _LoginComponentState extends State<LoginComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('login');
+  }
+
+}
