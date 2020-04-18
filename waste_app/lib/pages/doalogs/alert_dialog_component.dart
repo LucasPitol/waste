ae8b437738ac5ae9de53ad33b8c009592d11c66b
Pitol <lucascpitol0@gmail.com>
2020-04-18 17:23:11 -0300

    Refactor dialog

--- waste_app/lib/pages/doalogs/alert_dialog_component.dart
+++ waste_app/lib/pages/doalogs/alert_dialog_component.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
-class AlertDialogComponent extends StatelessWidget {
-
+class AlertDialogComponent extends StatefulWidget {
   final String title;
   final String content;
 
   AlertDialogComponent(this.title, this.content);
 
   @override
+  _AlertDialogComponentState createState() =>
+      _AlertDialogComponentState(title, content);
+}
+
+class _AlertDialogComponentState extends State<AlertDialogComponent> {
+  final String title;
+  final String content;
+
+  _AlertDialogComponentState(this.title, this.content);
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) => this._showDialog());
+  }
+
+  _showDialog() {
+    return showGeneralDialog(
+        barrierColor: Colors.black.withOpacity(0.5),
+        transitionBuilder: (context, a1, a2, widget) {
+          final curvedValue = Curves.easeInOutBack.transform(a1.value) - 1.0;
+          return Transform(
+            transform: Matrix4.translationValues(0.0, curvedValue * 200, 0.0),
+            child: Opacity(
+              opacity: a1.value,
+              child: AlertDialog(
+                shape: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0)),
+                title: Text(this.title),
+                content: Text(this.content),
+                actions: <Widget>[
+                  FlatButton(
+                    textColor: Colors.deepPurple,
+                    onPressed: closeDialog,
+                    child: Text('Ok'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        transitionDuration: Duration(milliseconds: 200),
+        barrierDismissible: false,
+        barrierLabel: '',
+        context: context,
+        pageBuilder: (context, animation1, animation2) {});
+  }
+
+  closeDialog() {
+    Navigator.pop(context, 'Ok');
+    Navigator.pop(context, 'Ok');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: AlertDialog(
-        title: Text(this.title),
-        content: Text(this.content),
-        actions: <Widget>[
-          FlatButton(
-            textColor: Colors.deepPurple,
-            onPressed: () => Navigator.pop(context, 'Ok'),
-            child: Text('Ok'),
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
