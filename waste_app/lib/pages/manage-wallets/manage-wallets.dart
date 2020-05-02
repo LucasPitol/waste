8863480af735e6dd220a03191ef6ed2042707276
Pitol <lucascpitol0@gmail.com>
2020-05-02 20:26:24 -0300

    DP-DEV-12 Tarefa 1: Progress

--- waste_app/lib/pages/manage-wallets/manage-wallets.dart
+++ waste_app/lib/pages/manage-wallets/manage-wallets.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:waste_app/models/wallet.dart';
+import 'package:waste_app/services/auth_service.dart';
+import 'package:waste_app/utils/constants.dart';
 
 import 'manage-wallets-app-bar.dart';
 
 class ManageWallets extends MaterialPageRoute<bool> {
   ManageWallets()
       : super(builder: (BuildContext context) {
+          var userDto = AuthService.currentUser;
+          List<Wallet> wallets;
+
           return StatefulBuilder(
               builder: (BuildContext context, StateSetter setState) {
             return Scaffold(
               appBar: ManageWalletsAppBar(context),
-              body: Text('data'),
+              body: Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.topRight,
+                      child: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          userDto.language == Constants.languages[0]
+                              ? 'Criar nova carteira'
+                              : 'Create new wallet',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             );
           });
         });
