22e6a1ea195adebd753e7a8c80f829c95b48c0fc
Pitol <lucascpitol0@gmail.com>
2020-05-05 00:09:02 -0300

    DP-DEV-10 tarefa 2: Progress date picker

--- waste_app/lib/pages/new-spend/new-spend-component.dart
+++ waste_app/lib/pages/new-spend/new-spend-component.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
 import 'package:waste_app/models/user_dto.dart';
 import 'package:waste_app/services/auth_service.dart';
 import 'package:waste_app/utils/constants.dart';
@@ -11,6 +13,25 @@ class NewSpendComponent extends StatefulWidget {
 
 class _NewSpendComponenState extends State<NewSpendComponent> {
   UserDto userDto = AuthService.currentUser;
+
+  DateTime spendDate = DateTime.now();
+
+  void _selectDate() {
+    DatePicker.showDateTimePicker(context,
+        theme: DatePickerTheme(
+          doneStyle: TextStyle(color: Colors.deepPurple),
+        ),
+        showTitleActions: true,
+        minTime: DateTime(1810, 1, 1),
+        maxTime: DateTime.now().add(
+          Duration(days: 35),
+        ),
+        locale: LocaleType.pt, onConfirm: (newDate) {
+      setState(() {
+        this.spendDate = newDate;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -98,6 +119,33 @@ class _NewSpendComponenState extends State<NewSpendComponent> {
                                   'Desperdício')
                               : Styles.getTextFieldDecorationUnderline('Waste'),
                     ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 20, bottom: 20, right: 10),
+                        child: Text(
+                          'Data:',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          _selectDate();
+                        },
+                        child: Container(
+                          margin:
+                              EdgeInsets.only(top: 20, bottom: 20, left: 10),
+                          child: Text(
+                            DateFormat.yMd(Constants.ptLanguage)
+                                .add_jm()
+                                .format(spendDate),
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
