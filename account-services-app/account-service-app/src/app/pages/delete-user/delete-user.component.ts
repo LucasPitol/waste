383d390470ea5daef0a89ba9fa35bc2a7f2abf1a
Pitol <lucascpitol0@gmail.com>
2020-07-02 22:43:47 -0300

    DP-DEV-15 tarefa 2: Progress

---
+++ account-services-app/account-service-app/src/app/pages/delete-user/delete-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'delete-user-component',
+    templateUrl: './delete-user.component.html',
+    styleUrls: ['./delete-user.component.css']
+})
+
+export class DeleteUserComponent {
+
+}
