69ce2ecf9e16572dba0ff1ba6466fd98ef63e3f3
Pitol <lucascpitol0@gmail.com>
2020-07-12 18:03:21 -0300

    DP-DEV-16 tarefa 2: Progress

--- account-services-app/account-service-app/src/app/pages/change-password/change-password.component.ts
+++ account-services-app/account-service-app/src/app/pages/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Utils } from 'src/app/utils/utils';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'change-password-component',
@@ -6,6 +8,28 @@ import { Component } from '@angular/core';
     styleUrls: ['./change-password.component.css']
 })
 
-export class ChangePasswordComponent {
+export class ChangePasswordComponent implements OnInit {
+
+    loading = true
+    uid: string
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+    ) {
+        this.uid = this.activatedRoute.snapshot.paramMap.get("uid")
+     }
+
+    ngOnInit() {
+        this.toggleLoading(false)
+    }
+
+    toggleLoading(value: boolean) {
+        this.loading = value
+    }
+
+    goHome() {
+        Utils.goHome(this.router)
+    }
 
 }
