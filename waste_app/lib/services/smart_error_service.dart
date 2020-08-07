27347c1155a79f642a946740dac2b8d2fc374c95
Pitol <lucascpitol0@gmail.com>
2020-08-07 20:55:05 -0300

    DP-DEV-21 tarefa 4

--- waste_app/lib/services/smart_error_service.dart
+++ waste_app/lib/services/smart_error_service.dart
@@ -1,3 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:waste_app/models/smart_error.dart';
+
 class SmartErrorService {
-  
+
+  final dbReference = Firestore.instance;
+
+  Future<void> saveError(SmartError errorDto) async {
+
+    Timestamp creationDate = Timestamp.fromDate(DateTime.now());
+
+    await dbReference.collection('errors').add({
+      'creationDate': creationDate,
+      'errorLog': errorDto.errorLog,
+      'feature': errorDto.feature,
+      'userId': errorDto.userId
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
 }
