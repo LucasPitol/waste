a9107d6367755eea4742fa4f32fe1a1b64263ad7
Pitol <lucascpitol0@gmail.com>
2020-08-12 21:04:41 -0300

    DP-DEV-24 tarefa 3

--- waste_app/lib/pages/profile/profile_component.dart
+++ waste_app/lib/pages/profile/profile_component.dart
@@ -176,6 +176,27 @@ class ProfileComponentState extends State<ProfileComponent> {
           width: 50,
         ),
       ),
+      children: [
+        Container(
+          child: Text(
+            '• Editar/excluir desperdícios',
+            style: TextStyle(color: Colors.grey.shade700),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 10, bottom: 10),
+          child: Text(
+            '• Tapa no layout',
+            style: TextStyle(color: Colors.grey.shade700),
+          ),
+        ),
+        Container(
+          child: Text(
+            '• Correção de bugs',
+            style: TextStyle(color: Colors.grey.shade700),
+          ),
+        ),
+      ],
     );
   }
 
