9988804b6734f34abd6707e0e205d0078c9de5ea
Pitol <lucascpitol0@gmail.com>
2020-08-15 11:37:46 -0300

    DP-DEV-23 tarefa 1

---
+++ waste_app/lib/pages/edit_spend/edit_spend_component.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditSpendComponent extends StatefulWidget {
+  @override
+  _EditSpendComponenState createState() => _EditSpendComponenState();
+}
+
+class _EditSpendComponenState extends State<EditSpendComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
