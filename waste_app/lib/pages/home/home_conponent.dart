3ef1c7742abb4977031082a6b17fe7ddf79a356c
Pitol <lucascpitol0@gmail.com>
2020-09-30 21:15:28 -0300

    DP-DEV-39 tarefa 1: progress

--- waste_app/lib/pages/home/home_conponent.dart
+++ waste_app/lib/pages/home/home_conponent.dart
@@ -16,15 +16,65 @@ class HomeComponentState extends State<HomeComponent> {
       statusBarIconBrightness: Brightness.light,
     ));
     return Scaffold(
-        backgroundColor: Styles.mainBackgroundColor,
-        resizeToAvoidBottomPadding: true,
-        body: SafeArea(
+      backgroundColor: Styles.mainBackgroundColor,
+      resizeToAvoidBottomPadding: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Container(
-            child: Text(
-              'Homee',
-              style: TextStyle(color: Colors.orange),
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(
+              children: [
+                Container(
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              'Rombado',
+                              style: TextStyle(
+                                color: Colors.grey.shade100,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              'Plano básico',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        alignment: Alignment.topRight,
+                        child: GestureDetector(
+                          onTap: () {
+                            print('settings');
+                          },
+                          child: Icon(
+                            Icons.settings,
+                            color: Colors.grey.shade100,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
